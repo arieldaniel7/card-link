73c93f8ba2bdc2209709c15b6cf59ffc6b894d78
Your Name <arieldaniel44@gmail.com>
2022-10-18 19:54:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function youtubeLink(){
+    window.open("https://www.youtube.com/channel/UCk8YPtyPV7z7RgP2MqFFjrQ")
+}
+
+function instagramLink(){
+    window.open("https://www.instagram.com/sushibond/")
+}
+
+function twitterLink(){
+    window.open("https://twitter.com/adariel_")
+}
+
+function githubLink(){
+    window.open("https://github.com/arieldaniel7")
+}
+
